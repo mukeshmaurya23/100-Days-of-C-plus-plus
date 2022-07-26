5e0f99021d92d28cbaaec3a211fb292c68f4c7e6
Mukesh Maury <mukeshmaurya9594@gmail.com>
2022-07-26 21:42:36 +0530

    first day adding syntax logic in c++ with some questions and answers

---
+++ Syntax.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+// Question:Why preProcessor does not have Semicolon at end
+// Answer:preProcessor start with # and semicolon is needed by compiler
+//        preProcessor are program that process our source code before compilation
+//        hence  it dosent required semicolon
+
+// functionality of cout and cin and many standard operation are include inside iostream header file
+
+// preprocessor file are comp
+using namespace std;
+// In c++ two variable with same name is not allowed to overcome this namespace is used
+// if you not  defined using namespace std then you have to write like eg
+// std::cout<<"Enter Name"
+// functionality of cout and cin are in iostream header file under namespace std thats why we need both
+// iostream and using namespace std;
+int main()
+{
+    cout << "Hello world!";
+    return 0;
+}
